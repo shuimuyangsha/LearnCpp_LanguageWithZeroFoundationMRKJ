d8702e357493dca5c5e415b29c26e94d8ab63f23
shuimuyangsha <1023178387@qq.com>
2021-02-08 18:06:34 +0800

    ~

--- Code/sl/02/01/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>                         // 包含头文件
-using namespace std;                        // 引入命名空间
-void main()
-{
-    char c1, c2;                            // 定义两个char类型的变量
-    c1 = 'a';                               // 将变量ch1赋值为'a'
-    c2 = 'b';                               // 将变量ch2赋值为'b'
-    printf("%c,%d\n%c,%d", c1, c1, c2, c2); // 分别公字符型和整型格式输出变量
-}
--- Code/sl/03/01/main.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-void main()
-{
-    int i=5,j=8,k=12,l=4,x1,x2;
-    x1=i>j&&k>l;               // 先进行“大于”和“小于”运算，再进行“与”运算
-    x2=!(i>j)&&k>l;            // 
-    printf("%d,%d\n",x1,x2);
-}
