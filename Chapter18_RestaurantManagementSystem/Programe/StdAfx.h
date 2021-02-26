2abf76a17fd378274c4b580251048943bcab7e0c
shuimuyangsha <1023178387@qq.com>
2021-02-26 11:34:17 +0800

    ~

---
+++ Chapter18_RestaurantManagementSystem/Programe/StdAfx.h
@@ -0,0 +1,35 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__9B887328_1300_4F88_B2A0_EBEFD1520770__INCLUDED_)
+#define AFX_STDAFX_H__9B887328_1300_4F88_B2A0_EBEFD1520770__INCLUDED_
+
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <afxwin.h>         // MFC core and standard components
+#include <afxext.h>         // MFC extensions
+#include <afxdisp.h>        // MFC Automation classes
+#include <afxdtctl.h>		// MFC support for Internet Explorer 4 Common Controls
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>			// MFC support for Windows Common Controls
+#endif // _AFX_NO_AFXCMN_SUPPORT
+//#import "C:\\Users\\Administrator\\Desktop\\project 2\\Programe\\msado15.dll" no_namespace rename("EOF","adoEOF")
+
+//#import "E:\\Code\\LeaningC\\LearnCpp_LanguageWithZeroFoundationMRKJ\\Chapter18_RestaurantManagementSystem\\Programe\\Debug\\msado15.dll" no_namespace rename("EOF","adoEOF")
+
+
+#import "C:\ForEden\MRKJ\\msado15.dll" no_namespace rename("EOF","adoEOF")
+
+
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__9B887328_1300_4F88_B2A0_EBEFD1520770__INCLUDED_)
