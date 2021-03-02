1e19f0e087af18afcf529ca88f09a0a0518a9fd8
shuimuyangsha <1023178387@qq.com>
2021-03-02 18:05:15 +0800

    ~

--- Code/SL/16/02/02/02cpp.cpp
+++ Code/SL/16/02/02/02cpp.cpp
@@ -1,26 +1,55 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+//int main()
+//{
+//	ofstream ofile;
+//	cout << "Create file1" << endl;
+//	ofile.open("test.txt");
+//	if (!ofile.fail())
+//	{
+//		ofile << "name1" << " ";
+//		ofile << "sex1" << " ";
+//		ofile << "age1";
+//		ofile.close();
+//		cout << "Create file2" << endl;
+//		ofile.open("test2.txt");
+//		if (!ofile.fail())
+//		{
+//			ofile << "name2" << " ";
+//			ofile << "sex2" << " ";
+//			ofile << "age2";
+//			ofile.close();
+//		}
+//	}
+//	return 0;
+//}
+
 int main()
 {
 	ofstream ofile;
-	cout << "Create file1" << endl;
-	ofile.open("test.txt");
-	if (!ofile.fail())
-	{
-		ofile << "name1" << " ";
-		ofile << "sex1" << " ";
-		ofile << "age1";
+	cout << "Create filel" << endl;
+	ofile.open("test3.txt");
+	if (!ofile.fail()) {
+		ofile << "name3" << " ";
+		ofile << "sex3" << "";
+		ofile << "age3";
 		ofile.close();
-		cout << "Create file2" << endl;
-		ofile.open("test2.txt");
-		if (!ofile.fail())
-		{
-			ofile << "name2" << " ";
-			ofile << "sex2" << " ";
-			ofile << "age2";
+
+		cout << "Create file4" << endl;
+		ofile.open("test4.txt");
+		if (!ofile.fail()) {
+			ofile << "name4" << " ";
+			ofile << "sex4" << " ";
+			ofile << "age4";
 			ofile.close();
 		}
 	}
+
 	return 0;
 }
+
+
+
+
+
